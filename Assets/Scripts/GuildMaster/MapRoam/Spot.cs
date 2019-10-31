11c1c0f8cd1debec8a6ea47c4c7beb8c44dd65e5
jaewoo.kim <jaewoo986@gmail.com>
2019-11-01 02:37:06 +0900

    Fix some names and folder structure.

---
+++ Assets/Scripts/GuildMaster/MapRoam/Spot.cs
@@ -0,0 +1,5 @@
+namespace GuildMaster.MapRoam {
+    public class Spot {
+        
+    }
+}
---
+++ Assets/Scripts/GuildMaster/TextBox.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GuildMaster
+{
+
+	public class TextBox : MonoBehaviour
+	{
+		// Start is called before the first frame update
+		void Start()
+		{
+
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
+	}
+
+}
