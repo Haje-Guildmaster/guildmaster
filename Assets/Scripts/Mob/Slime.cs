3121c047e1d64e6d83eb670d46cde6c9fc2a81fa
HAJE_honeyirene <eogjs99@gmail.com>
2019-11-05 02:11:44 +0900

    make dummy Mob - Slime

---
+++ Assets/Scripts/Mob/Slime.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GuildMaster
+{
+    public class Slime : MobBase
+    {
+        public override int HP { get; set; }
+        public override int ATK { get; set; }
+        public override int DEF { get; set; }
+        public override int SPD { get; set; }
+
+        // Initialize Slime's stat
+        public override void Init()
+        {
+            this.HP = 5;
+            this.ATK = 3;
+            this.DEF = 1;
+            this.SPD = 3;
+        }
+
+        // Slime's Skill Erode
+        public void Erode(CharacterBase enemy)
+        {
+            enemy.GetDamage(this.ATK);
+            Debug.Log("Slime use Skill, Erode");
+        }
+
+        // Slime's AI
+        public override void AI()
+        {
+            //Erode();
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
