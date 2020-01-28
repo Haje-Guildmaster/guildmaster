a17a95d0a3354a3576e3dd494eae8d7072b99ff9
jaewoo <jaewoo_kim@naver.com>
2020-01-28 21:30:24 +0900

    Add ScriptableObject QuestData.

--- Assets/Scripts/Quests/QuestData.cs
+++ Assets/Scripts/Quests/QuestData.cs
@@ -1,10 +1,19 @@
-﻿using UnityEngine;
+﻿using GuildMaster.Conditions;
+using UnityEngine;
     
 namespace GuildMaster.Quests
 {
     [CreateAssetMenu(fileName = "Quest", menuName = "ScriptableObjects/QuestData", order = 0)]
     public class QuestData: ScriptableObject
     {
+        [SerializeField] private Condition activationCondition;
+        [SerializeField] private Condition completionCondition;
+        [SerializeField] private string testTalkScript;
+        [SerializeField] private string testCompleteScript;
         
+        public Condition ActivationCondition => activationCondition;
+        public Condition CompletionCondition => completionCondition;
+        public string TestTalkScript => testTalkScript;
+        public string TestCompleteScript => testCompleteScript;
     }
 }
