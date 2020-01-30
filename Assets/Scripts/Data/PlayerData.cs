8e164145971923facb8233204a0fa50ec34036a2
jaewoo <jaewoo_kim@naver.com>
2020-01-30 14:34:54 +0900

    Enable PlayerData to check Conditions.

--- Assets/Scripts/Data/PlayerData.cs
+++ Assets/Scripts/Data/PlayerData.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using GuildMaster.Conditions;
 using GuildMaster.Quests;
 using UnityEngine;
 
@@ -10,7 +12,34 @@ namespace GuildMaster.Data
     [Serializable]
     public class PlayerData
     {
-        [SerializeField] private HashSet<QuestData> completedQuests;
-        [SerializeField] private HashSet<QuestData> currentQuests;
+        private HashSet<QuestData> _completedQuests = new HashSet<QuestData>();
+        private HashSet<Quest> _currentQuests = new HashSet<Quest>();
+        private int _level = 1;
+
+        public bool CheckCondition(Condition condition)
+        {
+            switch (condition)
+            {
+                case Condition.Always always:
+                    return always.isTrue;
+                case Condition.And and:
+                    return and.conditions.Aggregate(true, (calc, cond) => calc && CheckCondition(cond));
+                case Condition.Or or:
+                    return or.conditions.Aggregate(false, (calc, cond) => calc || CheckCondition(cond));
+                case Condition.CompletedQuest hasCompletedQuest:
+                    return _completedQuests.Contains(hasCompletedQuest.quest);
+                case Condition.LevelOver levelOver:
+                    return levelOver.level > _level;
+                default:
+                    throw new Exception($"Unexpected Condition: {condition}");
+            }
+        }
+
+        private static PlayerData _instance;
+
+        public static PlayerData Instance
+        {
+            get { return _instance = _instance ?? new PlayerData(); }
+        }
     }
 }
