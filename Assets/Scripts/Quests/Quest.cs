5b7d427c9249f0712b821dde05ffab2ba5a8c694
jaewoo <jaewoo_kim@naver.com>
2020-01-30 14:35:59 +0900

    Make class Quest store current progress and completion condition of the quest step.

--- Assets/Scripts/Quests/Quest.cs
+++ Assets/Scripts/Quests/Quest.cs
@@ -1,18 +1,65 @@
 ﻿using System;
+using System.Linq;
+using GuildMaster.Conditions;
+using GuildMaster.Data;
+using GuildMaster.Npcs;
 
 namespace GuildMaster.Quests
 {
+    using Script = String;
     // 퀘스트 진행상황을 저장함.
+    // QuestData의 iterator에 가까우나 QuestData안의 일부 정보를 저장하기에 QuestData가 수정되면 오류가 나기 쉽습니다.
     [Serializable]
     public class Quest
     {
-        private QuestData _questData;
-        private int _index = 0;
+        public readonly QuestData QuestData;
+        private int _index;
+        private Condition _stepCompleteCondition;
+        private bool _completed = false;
+        private NpcData _reportTo;
+        private Script _reportScript;
         
-        
+        public Condition StepCompleteCondition => _stepCompleteCondition;
+        public bool Completed => _completed;
+        public NpcData ReportTo => _reportTo;
+        public string ReportScript => _reportScript;
+
         public Quest(QuestData questData)
         {
-            this._questData = questData;
+            this.QuestData = questData;
+
+            _index = -1;
+            NextStep();
+        }
+
+        public bool TryCompleteStep(PlayerData playerData)
+        {
+            if (Completed) return false;
+            if (!playerData.CheckCondition(_stepCompleteCondition))
+                return false;
+            NextStep();
+            return true;
+        }
+
+        private void NextStep()
+        {
+            _index++;
+            var nextStep = QuestData.Steps.ElementAtOrDefault(_index);
+            if (nextStep == null)
+                MarkCompleted();
+            else
+                LoadQuestStep(nextStep);
+        }
+        private void LoadQuestStep(QuestData.Step step)
+        {
+            _stepCompleteCondition = step.completeCondition.GenerateCompletionCondition(PlayerData.Instance);
+            _reportScript = step.reportScript;
+            _reportTo = step.reportTo;
+        }
+
+        private void MarkCompleted()
+        {
+            _completed = true;
         }
     }
 }
