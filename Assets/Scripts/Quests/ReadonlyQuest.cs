6ee699db3b929b49d463ae848944f886a4dcd682
jaewoo <jaewoo_kim@naver.com>
2020-02-05 22:12:02 +0900

    Add ReadonlyQuest.

---
+++ Assets/Scripts/Quests/ReadonlyQuest.cs
@@ -0,0 +1,21 @@
+﻿using GuildMaster.Npcs;
+
+namespace GuildMaster.Quests
+{
+    public struct ReadonlyQuest
+    {
+        private readonly Quest _quest;
+        public ReadonlyQuest(Quest quest)
+        {
+            _quest = quest;
+        }
+
+        public NpcData Client => _quest.Client;
+        public int Index => _quest.Index;
+        public QuestStep CurrentStep => _quest.CurrentStep;
+        public QuestData QuestData => _quest.QuestData;
+        public int StepProgress => _quest.StepProgress;
+        public bool CanCompleteQuest => _quest.CanCompleteQuest;
+        public bool CanCompleteStep => _quest.CanCompleteStep;
+    }
+}
