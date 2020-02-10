a0938d715f2eb549c81298f35b839b8bac1e5bf0
jaewoo <jaewoo_kim@naver.com>
2020-02-10 23:38:54 +0900

    Add progress bar.

---
+++ Assets/Scripts/UI/ProgressBar.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace GuildMaster.UI
+{
+    public class ProgressBar: MonoBehaviour
+    {
+        public Image barImage;
+        public Text progressText;
+        
+        public void SetProgress(float progress)
+        {
+            barImage.fillAmount = progress;
+            progressText.text = $"{(int) (progress * 100)}/100";
+        }
+    }
+}
