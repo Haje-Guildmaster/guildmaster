0ac39a95a800a62e47b1b055b647412c814dedc1
jaewoo <jaewoo_kim@naver.com>
2020-02-26 18:30:04 +0900

    Add GiveRewards class for debugging purpose.

---
+++ Assets/Scripts/Tools/GiveRewards.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using GuildMaster.Data;
+using GuildMaster.Rewards;
+using UnityEngine;
+
+namespace GuildMaster.Tools
+{
+    // 디버깅용. 게임 시작 시 reward 들을 적용.
+    public class GiveRewards: MonoBehaviour
+    {
+        [SerializeReference][SerializeReferenceButton] public List<Reward> rewards;
+
+        private void Start()
+        {
+            foreach (var reward in rewards)
+                PlayerData.Instance.ApplyReward(reward);
+            Destroy(this);
+        }
+    }
+}
