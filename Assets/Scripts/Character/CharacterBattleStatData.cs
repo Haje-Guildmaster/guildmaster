3f79dcf488e1bfe3c3ecea7728c3ceec33a7e0cc
HAJE_honeyirene <eogjs99@gmail.com>
2020-03-03 11:53:21 +0900

    Add Character Stat Data

---
+++ Assets/Scripts/Character/CharacterBattleStatData.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public struct CharacterBattleStatData
+{
+    [SerializeField] private int atk;
+    [SerializeField] private int def;
+    [SerializeField] private int agi;
+    [SerializeField] private int _int;
+
+    private int ATK
+    {
+        get { return atk; }
+        set { atk = value; }
+    }
+    private int DEF
+    {
+        get { return def; }
+        set { def = value; }
+    }
+    private int AGI
+    {
+        get { return agi; }
+        set { agi = value; }
+    }
+    private int INT
+    {
+        get { return _int; }
+        set { _int = value; }
+    }
+}
