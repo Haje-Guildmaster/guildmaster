8a8375f6320a9f33e903ded542703d7d2a4b3067
jaewoo <jaewoo_kim@naver.com>
2020-03-09 18:56:58 +0900

    Extract the color changing part of ItemCategoryToggle and put it in independent class ToggleOnColor.

--- Assets/Scripts/UI/Inventory/ItemCategoryToggle.cs
+++ Assets/Scripts/UI/Inventory/ItemCategoryToggle.cs
@@ -1,26 +1,11 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 namespace GuildMaster.UI.Inventory
 {
-    [RequireComponent(typeof(Toggle))]
-    public class ItemCategoryToggle: MonoBehaviour
+    public class ItemCategoryToggle: ToggleOnColor
     {
         public InventoryWindow.ItemCategory category;
-        private ColorBlock _originalColor;
-        public ColorBlock selectedColor;
-        
-        private void Awake()
-        {
-            Toggle = GetComponent<Toggle>();
-            _originalColor = Toggle.colors;
-            Toggle.onValueChanged.AddListener(UpdateToggleColor);
-        }
-
-        private void UpdateToggleColor(bool isOn)
-        {
-            Toggle.colors = isOn ? selectedColor : _originalColor;
-        }
-        public Toggle Toggle { get; private set; }
     }
 }
