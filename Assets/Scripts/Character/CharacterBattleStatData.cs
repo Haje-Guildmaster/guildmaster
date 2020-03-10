c28f0e79bab9e348da8ae308db6f3b53b51fd1f3
jaewoo <jaewoo_kim@naver.com>
2020-03-10 23:46:56 +0900

    Change access modifiers of CharacterBattleStatData's properties from private to public.

--- Assets/Scripts/Character/CharacterBattleStatData.cs
+++ Assets/Scripts/Character/CharacterBattleStatData.cs
@@ -8,22 +8,22 @@ public struct CharacterBattleStatData
     [SerializeField] private int agi;
     [SerializeField] private int _int;
 
-    private int ATK
+    public int ATK
     {
         get { return atk; }
         set { atk = value; }
     }
-    private int DEF
+    public int DEF
     {
         get { return def; }
         set { def = value; }
     }
-    private int AGI
+    public int AGI
     {
         get { return agi; }
         set { agi = value; }
     }
-    private int INT
+    public int INT
     {
         get { return _int; }
         set { _int = value; }
