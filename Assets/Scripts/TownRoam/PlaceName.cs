d0a3c3d0e7af421e748b2604e287a1c05447299a
jaewoo.kim <jaewoo986@gmail.com>
2020-12-30 23:47:38 +0900

    Add enum PlaceName.

---
+++ Assets/Scripts/TownRoam/PlaceName.cs
@@ -0,0 +1,14 @@
+namespace GuildMaster.TownRoam
+{
+    public enum PlaceName
+    {
+        None,
+        Guild,
+        Tavern,
+        GatheringPlace,
+        Square,
+        Church,
+        Forge,
+        Stagecoach,
+    }
+}
