095d9586d5936490d4dbb22d4f933cd008c75be9
jaewoo.kim <jaewoo986@gmail.com>
2020-12-30 23:47:47 +0900

    기존 Place의 기능을 단순화한 Room 추가.

---
+++ Assets/Scripts/TownRoam/Room.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.Assertions;
+
+
+namespace GuildMaster.TownRoam
+{
+    public class Room : MonoBehaviour
+    {
+        [SerializeField] private SpriteRenderer _backgroundRenderer;
+        [field: SerializeField] public PlaceName BelongingPlace { get; private set; }
+
+        public Vector2 Size => _backgroundRenderer.bounds.size;
+        public Vector2 Center => _backgroundRenderer.transform.position;
+        // 일단 임시로 backgroundRenderer에서 모든 값을 가져오도록 했습니다.
+        // 추후에 직접 값 입력도 할 수 있도록 해야 하지 않을까 합니다.
+
+        private void Awake()
+        {
+            Assert.IsTrue(BelongingPlace != PlaceName.None);
+        }
+    }
+}
