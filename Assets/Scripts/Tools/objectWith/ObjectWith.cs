12c8455d9144c73197588e706aa7bfb214fb51a7
jaewoo.kim <jaewoo986@gmail.com>
2021-01-26 18:18:01 +0900

    Update ObjectWith's description.

--- Assets/Scripts/Tools/objectWith/ObjectWith.cs
+++ Assets/Scripts/Tools/objectWith/ObjectWith.cs
@@ -6,7 +6,8 @@ namespace GuildMaster.Tools
     /// <summary>
     /// interface T를 지닌 유니티 오브젝트에 대한 reference를 unity inspector에서 지정하기 위해 사용하는 클래스. <br/>
     /// interface T 멤버가 serialized 되야 할 경우, 이 클래스를 대신 멤버로 넣은 뒤 .Object 로 진짜 오브젝트에 접근하면 됨. <br/>
-    /// 유니티 오브젝트(UnityEngine.Object를 상속한 클래스들)에 대한 링크만 가능합니다.
+    /// 유니티 오브젝트(UnityEngine.Object를 상속한 클래스들)에 대한 링크만 가능합니다. <br/>
+    /// inspector에서 GameObject를 끌어오는 걸로는 연결이 안 되니 inspector 두 개 띄워두고 컴포넌트를 직접 끌어 와야 함.
     /// </summary>
     /// <typeparam name="T"> 이 오브젝트가 지닌 유니티 오브젝트에 대한 reference가 지니는 타입. </typeparam>
     [Serializable]
