b4bfa737570a3bd10af0f4311ebcd403890ae953
jaewoo.kim <jaewoo986@gmail.com>
2021-01-28 14:26:10 +0900

    Fix BasicCharacterStatView.

--- Assets/Scripts/Windows/general/BasicCharacterStatView.cs
+++ Assets/Scripts/Windows/general/BasicCharacterStatView.cs
@@ -6,10 +6,10 @@ namespace GuildMaster.Windows
 {
     public class BasicCharacterStatView : MonoBehaviour, IValueView<Character.CharacterStat>
     {
-        [SerializeField] private ObjectWith<IValueView<int>> _atkView;
-        [SerializeField] private ObjectWith<IValueView<int>> _defView;
-        [SerializeField] private ObjectWith<IValueView<int>> _agiView;
-        [SerializeField] private ObjectWith<IValueView<int>> _intView;
+        [SerializeField] private ObjectWith<IValueView<Box<int>>> _atkView;
+        [SerializeField] private ObjectWith<IValueView<Box<int>>> _defView;
+        [SerializeField] private ObjectWith<IValueView<Box<int>>> _agiView;
+        [SerializeField] private ObjectWith<IValueView<Box<int>>> _intView;
         public void SetValue(Character.CharacterStat stat)
         {
             _atkView.Object.SetValue(stat.Atk);
