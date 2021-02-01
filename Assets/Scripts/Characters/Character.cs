1d37a0ef7c790bc61d3671fa190b5c0b67849997
Adlet <eypk777@gmail.com>
2021-02-01 18:48:43 +0900

    PR 수정

--- Assets/Scripts/Characters/Character.cs
+++ Assets/Scripts/Characters/Character.cs
@@ -3,6 +3,7 @@ using System.Collections.ObjectModel;
 using System.Collections.Generic;
 using GuildMaster.Databases;
 using GuildMaster.Tools;
+using System.Linq;
 
 namespace GuildMaster.Characters
 {
@@ -14,14 +15,30 @@ namespace GuildMaster.Characters
         public Character(CharacterCode code)
         {
             void InvokeChanged() => Changed?.Invoke();
-            _loyalty.Changed += InvokeChanged;
+            _loyaltyInnerValue.Changed += InvokeChanged;
+            _usingNameIndex.Changed += InvokeChanged;
             _hp.Changed += InvokeChanged;
             _stamina.Changed += InvokeChanged;
-            _usingNameIndex.Changed += InvokeChanged;    
+            _level.Changed += InvokeChanged;
+            _xp.Changed += InvokeChanged;
+            _hpChange.Changed += InvokeChanged;
+            _staminaChange.Changed += InvokeChanged;
+            _strengthChange.Changed += InvokeChanged;
+            _trickChange.Changed += InvokeChanged;
+            _wisdomChange.Changed += InvokeChanged;
             
             _code = code;
             _hp.Value = StatMaxHpList[1];
             _stamina.Value = StatMaxStaminaList[1];
+
+            //스텟 List 길이 MaxLevel과 비교
+            var levelBoundList = new List<ReadOnlyCollection<int>> { StatMaxHpList, StatMaxStaminaList, StatStrengthList, StatTrickList, StatWisdomList };
+            string errormessage = "";
+            foreach(var list in levelBoundList)
+            {
+                if (list.Count <= MaxLevel) errormessage += $"{nameof(list)}의 길이: {list.Count}으로 Unity 상에서 MaxLevel({MaxLevel})보다 적게 설정되어있음.\n";
+            }
+            if (errormessage != null) throw new Exception(errormessage.Trim());
         }
         //보정값 안 붙는 애들
         public string UsingName => NameList[_usingNameIndex].Trim();                // 현재 이름
@@ -29,7 +46,7 @@ namespace GuildMaster.Characters
         public bool KnowUseRealName => _usingNameIndex == NameList.Count;           // 현재 실제 이름을 사용중인지
         public CharacterStaticData StaticData => CharacterDatabase.Get(_code);      // 캐릭터 코드
         public ReadOnlyCollection<Trait> ActiveTraits => StaticData.BasicData.ActiveTraits.AsReadOnly();       // 캐릭터 특성
-        public int MaxLoyalty => StaticData.StatData.MaxLoyalty;                    // 충성도 최대 값 (내부적으로 100)
+        public int MaxLoyalty => StaticData.StatData.MaxLoyalty;                    // 외부로 보이는 충성도 최대 값 (아래 Loyalty 참고)
         //보정값 안 붙는 애들 private
         private readonly CharacterCode _code;
         private readonly ChangeTrackedValue<int> _usingNameIndex = new ChangeTrackedValue<int>(0); // 현재 나타날 이름의 index
@@ -37,7 +54,7 @@ namespace GuildMaster.Characters
         private readonly ChangeTrackedValue<int> _stamina = new ChangeTrackedValue<int>();
         private readonly ChangeTrackedValue<int> _level = new ChangeTrackedValue<int>(1);
         private readonly ChangeTrackedValue<int> _xp = new ChangeTrackedValue<int>(0);
-        private readonly ChangeTrackedValue<int> _loyalty = new ChangeTrackedValue<int>(0);
+        private readonly ChangeTrackedValue<int> _loyaltyInnerValue = new ChangeTrackedValue<int>(0);
         private ReadOnlyCollection<string> NameList => StaticData.BasicData.NameList.AsReadOnly();
         private ReadOnlyCollection<int> LevelupXP => StaticData.StatData.LevelupXP.AsReadOnly();
         private int MaxLevel => StaticData.StatData.MaxLevel;
@@ -111,17 +128,21 @@ namespace GuildMaster.Characters
             {
                 while (value >= LevelupXP[_level])
                 {
-                    _xp.Value = value - LevelupXP[_level];
+                    value -= LevelupXP[_level];
                     Level += 1;
                 }
                 _xp.Value = Math.Min(0, value);
             }
         }
         public int CurrentLevelupXP => LevelupXP[_level];                           // 현재 레벨업에 필요한 경험치 값
-        public int Loyalty  
+        /// <summary>
+        /// 충성도 변경시 LoyaltyInnerValue를 변경해주세요.
+        /// </summary>
+        public int Loyalty => (_loyaltyInnerValue.Value + 9) / 10; //보여지는 Loyalty는 loyaltyInnerValue를 올림하여 10으로 나눈 몫.
+        public int LoyaltyInnerValue
         {
-            get => (_loyalty.Value + 9) / 10;
-            set => _loyalty.Value = Math.Min(MaxLoyalty, Math.Max(value, 0));
+            get => _loyaltyInnerValue.Value;
+            set => _loyaltyInnerValue.Value = Math.Min(MaxLoyalty * 10, Math.Max(value, 0));
         }
     }
 }
