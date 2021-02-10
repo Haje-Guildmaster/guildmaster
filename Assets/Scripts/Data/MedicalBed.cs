3f1f249af4e632f0603e990d6d546bc82847452b
jaewoo.kim <jaewoo986@gmail.com>
2021-02-10 12:34:10 +0900

    Medicalbed 중복방지

--- Assets/Scripts/Data/MedicalBed.cs
+++ Assets/Scripts/Data/MedicalBed.cs
@@ -24,8 +24,20 @@ namespace GuildMaster.Data
         /// <returns></returns>
         public Character PutCharacter(int index, Character character)
         {
+            // 원래 있던 애 가져오기
             var prev = _onBeds[index];
+
+            // 있는 Bed중 새로 넣는 캐릭터가 이미 있으면 쫓아냄(중복방지)
+            if (character != null)
+            {
+                for (var i=0; i<_onBeds.Length; i++)
+                    if (_onBeds[i] == character)
+                        _onBeds[i] = null;
+            }
+            
+            // 새로운 캐릭터 지정.
             _onBeds[index] = character;
+
             Changed?.Invoke();
             return prev;
         }
