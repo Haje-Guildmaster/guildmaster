cc4ed409fbba58c62d747ed3b9c5d9d821dd3191
Adlet <eypk777@gmail.com>
2021-02-22 14:39:28 +0900

    ShopItemIcon 추가 및 prefab 적용

---
+++ Assets/Scripts/Windows/Inventory/ShopItemIcon.cs
@@ -0,0 +1,48 @@
+﻿
+using GuildMaster.Databases;
+using GuildMaster.Items;
+using GuildMaster.Tools;
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace GuildMaster.Windows
+{
+    public class ShopItemIcon : ItemIcon
+    {
+        [SerializeField] private Text _itemCostLabel;
+
+        public ShopItemIcon(Item item, int number, int index, int cost) 
+            : base(item, number, index)
+        {
+            UpdateAppearance(item, number, index, cost);
+        }
+
+        public void UpdateAppearance(Item item, int number, int index, int cost)
+        {
+            if (item == null || number == 0)
+            {
+                _item = null;
+                _number = 0;
+                _index = index;
+                _cost = cost;
+                _itemImage.sprite = (Sprite)null;
+                _itemNumberLabel.text = "";
+                _itemCostLabel.text = "";
+            }
+            else
+            {
+                _item = item;
+                _number = number;
+                _index = index;
+                _cost = cost;
+                _itemImage.sprite = ItemDatabase.Get(_item.Code).ItemImage;
+                _itemNumberLabel.text = _number.ToString();
+                _itemCostLabel.text = _cost.ToString();
+            }
+            return;
+        }
+        private int _cost;
+    }
+}
