00f258277c2755f0f1c9e7a5d25c3a0363ed22dc
jaewoo.kim <jaewoo986@gmail.com>
2021-03-05 18:09:01 +0900

    Refactor ItemIcon.

--- Assets/Scripts/Windows/Inventory/ItemIcon.cs
+++ Assets/Scripts/Windows/Inventory/ItemIcon.cs
@@ -1,95 +1,25 @@
-
-using GuildMaster.Databases;
-using GuildMaster.Items;
 using GuildMaster.Tools;
-using System;
+using JetBrains.Annotations;
 using UnityEngine;
-using UnityEngine.EventSystems;
+using UnityEngine.Assertions;
 using UnityEngine.UI;
 
 namespace GuildMaster.Windows
 {
-    public class ItemIcon : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IDragHandler, IBeginDragHandler, IEndDragHandler, IDropHandler, IPointerClickHandler
+    public sealed class ItemIcon : MonoBehaviour
     {
-        public event Action<Item> PointerEntered;
-        public event Action PointerExited;
-        public event Action<PointerEventData, int> BeginDrag;
-        public event Action EndDrag;
-        public event Action<PointerEventData> Drag;
-        public event Action<PointerEventData, int> Drop;
-        public event Action<Item, int> Click;
+        [field: SerializeReference] public ItemStackView ItemStackView { get; private set; }
 
-        [SerializeField] private Image _itemImage;
-        [SerializeField] private Text _itemNumberLabel;
+        [CanBeNull] public PointerEnterExitForwarder PointerEnterExitEvents { get; private set; }
+        [CanBeNull] public Button Button { get; private set; }
+        [CanBeNull] public DragForwarder DragForwarder { get; private set; }
 
-        ItemIcon(Item _item, int _number, int _index)
+
+        private void Awake()
         {
-            UpdateAppearance(_item, _number, _index);
+            PointerEnterExitEvents = GetComponent<PointerEnterExitForwarder>();
+            Button = GetComponent<Button>();
+            DragForwarder = GetComponent<DragForwarder>();
         }
-
-        public void UpdateAppearance(Item _item, int _number, int _index)
-        {
-            if (_item == null || _number == 0)
-            {
-                this._item = null;
-                this._number = 0;
-                this._index = _index;
-                _itemImage.sprite = (Sprite)null;
-                _itemNumberLabel.text = "";
-            }
-            else
-            {
-                this._item = _item;
-                this._number = _number;
-                this._index = _index;
-                _itemImage.sprite = ItemDatabase.Get(_item.Code).ItemImage;
-                _itemNumberLabel.text = _number.ToString();
-            }
-            return;
-        }
-
-        public void ItemIconOnOff(bool onoff)
-        {
-            if (onoff) gameObject.GetComponent<Image>().color = Color.white;
-            else gameObject.GetComponent<Image>().color = Color.red;
-        }
-
-        public void OnPointerEnter(PointerEventData eventData)
-        {
-            PointerEntered?.Invoke(_item);
-        }
-
-        public void OnPointerExit(PointerEventData eventData)
-        {
-            PointerExited?.Invoke();
-        }
-
-        public void OnBeginDrag(PointerEventData eventData)
-        {
-            BeginDrag?.Invoke(eventData, _index);
-        }
-
-        public void OnDrag(PointerEventData eventData)
-        {
-            Drag?.Invoke(eventData);
-        }
-
-        public void OnEndDrag(PointerEventData eventData)
-        {
-            EndDrag?.Invoke();
-        }
-
-        public void OnDrop(PointerEventData eventData)
-        {
-            Drop?.Invoke(eventData, _index);
-        }
-
-        public void OnPointerClick(PointerEventData eventData)
-        {
-            Click?.Invoke(_item, _number);
-        }
-
-        private Item _item = null;
-        private int _number = 0, _index;
     }
 }
