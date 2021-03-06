72cd4a9c3138a09e90c5f1bf4730deb8bf2bf0a1
jaewoo.kim <jaewoo986@gmail.com>
2021-03-06 16:35:35 +0900

    Refactor PlayerInventoryView(renamed from PlayerItemListView)

---
+++ Assets/Scripts/Windows/Inventory/PlayerInventoryView.cs
@@ -0,0 +1,24 @@
+﻿using GuildMaster.Data;
+using System;
+using UnityEngine;
+
+namespace GuildMaster.Windows
+{
+    public class PlayerInventoryView: MonoBehaviour
+    {
+        [SerializeField] private AutoRefreshedInventoryView _inventoryView;
+        
+        public void ChangeCategory(PlayerInventory.ItemCategory category)
+        {
+            _inventoryView.SetInventory(_playerInventory.GetInventory(category));
+        }
+
+        public void SetPlayerInventory(PlayerInventory playerInventory)
+        {
+            this._playerInventory = playerInventory;
+            ChangeCategory(PlayerInventory.ItemCategory.Equipable);
+        }
+        private PlayerInventory _playerInventory;
+    }
+
+}
